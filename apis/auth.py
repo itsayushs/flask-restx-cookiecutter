c0ef949a618dba40ee9bacd4ae424872a7f32a42
itsayushs <ayushstarksharma@gmail.com>
2020-05-25 20:31:31 +0530

    Token is Generated from Payload

--- apis/auth.py
+++ apis/auth.py
@@ -1,9 +1,19 @@
-from flask_restx import Resource, Namespace
+from flask_restx import Resource, Namespace, fields
+from flask_jwt_extended import create_access_token
 
 api = Namespace('User', description='User Auth JWT')
+userpayload = api.model('userdata', {
+    'username': fields.String,
+    'password': fields.String,
+    'role': fields.String,
+})
 
 
 @api.route('/login')
 class userLogin(Resource):
-    def get(self):
-        return {'Register': 'DummyCode'}
+    @api.expect(userpayload)
+    def post(self):
+        # userName = api.payload['username']
+        # password = api.payload['password']
+        access_token = create_access_token(identity=api.payload)
+        return {'JWTToken': access_token}
--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from apis import api
 
 app = Flask(__name__)
+JWTManager(app)
+app.config['JWT_SECRET_KEY'] = 'ayush-is-daddy'
 api.init_app(app)
 
 if __name__ == '__main__':
